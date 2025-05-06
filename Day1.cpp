3e88c92bf6bb86a3e5ca12213c0589b7091a0d0b
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-05-06 18:51:26 +0530

    completed day 1 of dsa

---
+++ Day1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Basic Print and I/O
+
+  // cout << "Enter a number: " << endl;
+  // int num;
+
+  // cin >> num;
+
+  // cout << "The number you entered is: " << num     << endl;
+
+  // Data Types And Variables
+
+  // int num = 10;
+  // cout << num << endl;
+
+  // cout << num + "10" << endl;
+
+  // char cc = 'a';
+  // cout << cc << endl;
+
+  // bool isMarrid = true;
+  // cout << isMarrid << endl;
+
+  // float f = 1.2;
+  // cout << f << endl;
+
+  // double d = 1.23;
+  // cout << d << endl;
+
+  // long l = 123456789;
+  // cout << l << endl;
+
+  // long long ll = 123456789123456789;
+  // cout << ll << endl;
+
+  // short s = 123;
+  // cout << s << endl;
+
+  // Operators
+
+  // int a = 10;
+  // int b = 20;
+  // cout << a + b << endl;
+  // cout << a - b << endl;
+  // cout << a * b << endl;
+  // cout << a / b << endl;
+  // cout << a % b << endl;
+  // cout << a++ << endl;
+  // cout << a-- << endl;
+  // cout << ++a << endl;
+  // cout << --a << endl;
+  // cout << (a == b) << endl;
+  // cout << (a != b) << endl;
+  // cout << (a > b) << endl;
+  // cout << (a < b)  << endl;
+  // cout << (a >= b) << endl;
+  // cout << (a <= b) << endl;
+  // cout << (a && b) << endl;
+  // cout << (a || b) << endl;
+
+  // conditional statements
+
+  // int broNum;
+
+  // cin >> broNum;
+
+  // if (broNum == 0) {
+  //   cout << "Done" << endl;
+  // } else {
+  //   cout << "Nope.." << endl;
+  // }
+
+  // Loop And Pattern problems
+
+  // Soild Rectangle
+
+  // int row, col;
+
+  // for (row = 0; row < 3; row++) {
+  //   for (col = 0; col < 5; col++) {
+  //     cout << "*" ;
+  //   }
+  //   cout << endl;
+  // }
+
+  // solid square
+
+  // int row, col;
+  // for (row = 0; row < 4; row++) {
+  //   for (col = 0; col < 4; col++) {
+  //     cout << "*";
+  //   }
+  //   cout << endl;
+  // }
+
+  // Howllow square...
+  // int row, col;
+  // cout << "Enter no of rows :" << endl;
+  // cin >> row;
+  // cout << "Enter no of columns :" << endl;
+  // cin >> col;
+
+  // for (int f = 0; f < row; f++) {
+  //   if (f == 0 || f == row - 1) {
+  //     for (int c = 0; c < col; c++) {
+  //       cout << "* ";
+  //     }
+  //   } else {
+  //     cout << "* ";
+  //     for (int i = 0; i < col - 2; i++) {
+  //       cout << "  ";
+  //     }
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // for (int r = 0; r < 5; r++) {
+  //   for (int v = 0; v < r + 1; v++) {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // for (int r = 1; r < 6; r++) {
+  //   for (int n = 1; n < r + 1; n++) {
+  //     cout << n;
+  //   }
+  //   cout << endl;
+  // }
+
+  // overall today learning -> Variables , datatypes , operators and loops then i done some easy pattern questions
+  // by the way i already know coding in python that because i moved so fast in this....
+
+}
