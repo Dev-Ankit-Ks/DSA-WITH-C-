8218bbb0ac93c13c40e1c1d6ac095a9f87cac150
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-05-07 20:59:57 +0530

    Day 2 of dsa

---
+++ Day2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // gratest of 3 num
+  // int a, b, c;
+  // cin >> a >> b >> c;
+  // if (a > b && a > c) {
+  //   cout << a << endl;
+  // } else if (b > c) {
+  //   cout << b << endl;
+  // } else {
+  //   cout << c << endl;
+  // }
+
+  // hollow rectangle
+  // int row, col;
+  // cin >> row >> col;
+
+  // for (int roww = 0; roww < row; roww++) {
+  //   for (int coll = 0; coll < col; coll++) {
+  //     if (roww == 0 || roww == row - 1 || coll == 0 || coll == col - 1) {
+  //       cout << "* ";
+  //     } else {
+  //       cout << "  ";
+  //     }
+  //   }
+  //   cout << endl;
+  // }
+
+  // inverted pyramid
+  //   int n;
+  //   cin >> n;
+
+  //   for (int row = 0; row < n; row++) {
+  //     for (int col = 0; col < n - row; col++) {
+  //       cout << "* ";
+  //     }
+  //     cout << endl;
+  //   }
+
+  // int n;
+  // cin >> n;
+
+  // hollow inverted pyramid..
+  //  for (int row = 0; row < n; row++) {
+  //    for (int col = 0; col < n; col++) {
+  //      if (row == 0 || col == 0 || col == n - row - 1) {
+  //        cout << "* ";
+  //      } else {
+  //        cout << "  ";
+  //      }
+  //    }
+  //    cout << endl;
+  //  }
+
+  // pyramid
+  //  int n;
+  //  cin >> n;
+  //  for (int row = 0; row < n; row++) {
+  //    for (int col = 0; col < n - row - 1; col++) {
+  //      cout << " ";
+  //    }
+  //    for (int j = 0; j < row + 1; j++) {
+  //      cout << "* ";
+  //    }
+  //    cout << endl;
+  //  }
+
+  // iverted pyramid
+
+  // int n;
+  // cin >> n;
+  // for (int row = 0; row < n; row++) {
+  //   for (int col = 0; col < row; col++) {
+  //     cout << " ";
+  //   }
+  //   for (int j = 0; j < n - row; j++) {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+}
