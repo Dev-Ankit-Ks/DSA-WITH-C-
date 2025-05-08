3233b17e4ce08b6559e25f18c59c6beed25b48b9
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-05-08 22:21:49 +0530

    day 3 done

---
+++ Day3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // int n;
+  // cin >> n;
+
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = n; j > i; j--) {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+
+  //
+
+  // bitwise operators
+  // bool a = true;
+  // bool b = false;
+
+  // cout << a << endl;
+
+  // a = a << 2;
+
+  // cout << a;
+
+  // increment and decrement operators..
+
+  // cout << (a | b);
+
+  // left shift and   right shift
+
+  // int a = 20;
+
+  // int a = 23;
+  // cout << a++ << endl;
+  // cout << a;
+
+  // done some problme roughly..
+
+  // functions...
+
+}
