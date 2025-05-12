b9507ba32c7c8da309508d112394dc27807c29ca
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-05-12 23:11:14 +0530

    Day 6 arrays

---
+++ Day6.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main() {
+
+  // int n;
+  // cout << "Enter size of array: " << endl;
+  // cin >> n;
+  // int arr[n];
+  // cout << "Enter the values of array: " << endl;
+  // for (int i = 0; i < n; i++) {
+  //   cin >> arr[i];
+  // }
+
+  // cout << "Values of array" << endl;
+
+  // for (int i = 0; i < n; i++) {
+  //   cout << 2 * arr[i] << ' ';
+  // }
+
+  //---------------------------------------------------
+
+  // int arr[5] = {23, 34, 2, 24, 2};
+
+  // for (int i = 0; i < 5; i++) {
+  //   arr[i] = 1;
+  // }
+
+  // for (int i = 0; i < 5; i++) {
+  //   cout << arr[i] << ' ';
+  // }
+
+  //------------------------------------------------------
+
+  // linear search
+
+  // int arr[5] = {3, 53, 9, 32, 13};
+  // for (int i = 0; i < 5; i++) {
+  //   if (arr[i] == 32) {
+  //     cout << "Yes 32 in this array" << endl;
+  //     break;
+  //   } else {
+  //     continue;
+  //   }
+  // }
+
+  //------------------------------------------------------
+
+  // counting 0 and 1 in array
+  // int arr[6] = {1, 0, 1, 1, 1, 0};
+  // int countOne = 0, countZero = 0;
+
+  // for (int i = 0; i < 6; i++) {
+  //   if (arr[i] == 1) {
+  //     countOne++;
+  //   } else {
+  //     countZero++;
+  //   }
+  // }
+  // cout << "No of ones in array is " << countOne << endl;
+  // cout << "No of zeros in array is " << countZero << endl;
+
+  //------------------------------------------------------------
+
+  // find max num in array
+
+  // int arr[5] = {2, 90, 2222, 39, 100};
+
+  // int maxi = INT_MIN;
+
+  // for (int i = 0; i < 5; i++) {
+  //   if (arr[i] > maxi) {
+  //     maxi = arr[i];
+  //   }
+  // }
+  // cout << maxi << endl;
+
+  //-----------------------------------------------------------
+
+  // first and last pointer in array(Doubt)
+
+  // int arr[6] = {1, 4, 2, 2, 54, 52};
+
+  // int startIdx = 0;
+  // int endingIdx = 5;
+
+  // while (true) {
+  //   if (startIdx > endingIdx) {
+  //     break;
+  //   }
+  //   if (startIdx == endingIdx) {
+  //     cout << arr[startIdx] << ' ';
+  //   } else {
+  //     cout << arr[startIdx] << " ";
+  //     cout << arr[endingIdx] << " ";
+  //   }
+  //   startIdx++;
+  //   endingIdx--;
+  // }
+  //--------------------------------------------------------
+
+  // int arr[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+
+  // int size = 8;
+  // int start = 0;
+  // int end = size - 1;
+
+  // while (start <= end) {
+  //   swap(arr[start], arr[end]);
+  //   start++;
+  //   end--;
+  // }
+
+  // for (int i = 0; i < size; i++) {
+  //   cout << arr[i] << ' ';
+  // }
+
+  //-----------------------Thats for today!!---------------------------------
+}
